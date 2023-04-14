2bab8d0daaef1a2077a32e7a717754dbd0c811ba
Matheus3DD4 <matheus3dd4@gmail.com>
2023-04-14 16:35:57 -0300

    black

--- livraria/migrations/0005_livro.py
+++ livraria/migrations/0005_livro.py
@@ -5,22 +5,48 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('livraria2', '0004_alter_autor_options'),
+        ("livraria2", "0004_alter_autor_options"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Livro',
+            name="Livro",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('titulo', models.CharField(max_length=25)),
-                ('isbn', models.CharField(blank=True, max_length=32, null=True)),
-                ('quantidade', models.IntegerField(blank=True, default=0, null=True)),
-                ('preco', models.DecimalField(blank=True, decimal_places=2, default=0, max_digits=7, null=True)),
-                ('categoria', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='livros', to='livraria2.categoria')),
-                ('editora', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='livros', to='livraria2.editora')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("titulo", models.CharField(max_length=25)),
+                ("isbn", models.CharField(blank=True, max_length=32, null=True)),
+                ("quantidade", models.IntegerField(blank=True, default=0, null=True)),
+                (
+                    "preco",
+                    models.DecimalField(
+                        blank=True, decimal_places=2, default=0, max_digits=7, null=True
+                    ),
+                ),
+                (
+                    "categoria",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="livros",
+                        to="livraria2.categoria",
+                    ),
+                ),
+                (
+                    "editora",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.PROTECT,
+                        related_name="livros",
+                        to="livraria2.editora",
+                    ),
+                ),
             ],
         ),
     ]
